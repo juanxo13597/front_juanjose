9f3e1c36eb597030ff8b9cad07e2950cb527f7f6
juanxo13597 <juanxo97master@gmail.com>
2023-02-16 21:08:17 +0100

    fix deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4200;
 
-app.use("/", express.static("../dist/front_juanjose"));
+app.use("/", express.static(__dirname + "/dist/front_juanjose"));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
