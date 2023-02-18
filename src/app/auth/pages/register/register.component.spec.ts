9c76ce2d54a9b14a7069a9be71ce1c7b8b584fe9
juanxo13597 <juanxo97master@gmail.com>
2023-02-18 14:03:12 +0100

    100 coverage test

--- src/app/auth/pages/register/register.component.spec.ts
+++ src/app/auth/pages/register/register.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jasmine/prefer-toHaveBeenCalledWith */
 import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
@@ -6,11 +7,13 @@ import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthService } from '../../services/auth.service';
 import { of } from 'rxjs';
+import { constants } from 'src/app/shared/constants/constants';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
   let service: AuthService;
+  const date = new Date();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -33,7 +36,7 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('register all success', () => {
+  it('register all success validation', () => {
     component.registerForm.patchValue({
       name: 'test',
       surname: 'test',
@@ -70,9 +73,72 @@ describe('RegisterComponent', () => {
       fixture.nativeElement.querySelector('#surname');
 
     expect(surnameEL.classList.contains('ng-valid')).toBeTrue();
+  });
 
-    spyOn(service, 'register').and.returnValue(of({ email: 'email@email.es' }));
+  it('register all success logic', () => {
+    jasmine.clock().install();
+    spyOn(component.registerForm, 'reset').and.callThrough();
+    spyOn(service, 'register').and.returnValue(
+      of({
+        email: 'email@email.es',
+        id: 1,
+        name: 'test',
+        surname: 'test',
+        created_at: date,
+        updated_at: date,
+      })
+    );
 
     component.submit();
+
+    expect(service.register).toHaveBeenCalled();
+
+    setTimeout(() => {
+      expect(component.registerForm.reset).toHaveBeenCalled();
+    }, constants.setTimeOut + 1000);
+    jasmine.clock().tick(10000);
+    jasmine.clock().uninstall();
+  });
+
+  it('register all error logic', () => {
+    spyOn(service, 'register').and.returnValue(
+      of({
+        email: 'email@email.es',
+        id: 1,
+        name: 'test',
+        surname: 'test',
+        created_at: date,
+        updated_at: date,
+        response: 'error',
+        error: 'error',
+        status: 401,
+        message: 'Email already exists',
+      })
+    );
+
+    component.submit();
+
+    expect(service.register).toHaveBeenCalled();
+  });
+
+  it('register all error logic 2', () => {
+    spyOn(service, 'register').and.returnValue(
+      of({
+        email: 'email@email.es',
+        id: 1,
+        name: 'test',
+        surname: 'test',
+        created_at: date,
+        updated_at: date,
+        response: 'error',
+        error: 'error',
+        status: 401,
+        message: 'Password confirmation does not match',
+      })
+    );
+
+    component.submit();
+
+    expect(service.register).toHaveBeenCalled();
   });
 });
