26f3283e65affc860e4264273c0a959a4a0b93ba
juanxo13597 <juanxo97master@gmail.com>
2023-02-20 13:17:44 +0100

    fix module

--- src/app/developments/todolist/todolist.module.ts
+++ src/app/developments/todolist/todolist.module.ts
@@ -1,18 +1,16 @@
-import { TranslateModule } from '@ngx-translate/core';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { TodolistRoutingModule } from './todolist-routing.module';
 import { TodolistComponent } from './todolist/todolist.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 /** todolist module */
 @NgModule({
   declarations: [TodolistComponent],
   imports: [
     CommonModule,
-    BrowserModule,
     TodolistRoutingModule,
     TranslateModule,
     ReactiveFormsModule,
